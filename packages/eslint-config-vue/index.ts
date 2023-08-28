288b9a43fa54c85afdc1322af3105fb2e41aa0cc
LoTwT <709937065@qq.com>
2023-08-28 16:11:26 +0800

    feat: eslint-config-vue

---
+++ packages/eslint-config-vue/index.ts
@@ -0,0 +1,4 @@
+import { vue } from "./src/vue"
+
+export { vue }
+export default vue
---
+++ packages/eslint-config-vue/shims.d.ts
@@ -0,0 +1,7 @@
+declare module "@sxzz/eslint-config" {
+  import { type FlatESLintConfigItem } from "eslint-define-config"
+
+  const vue: FlatESLintConfigItem[]
+
+  export { vue }
+}
