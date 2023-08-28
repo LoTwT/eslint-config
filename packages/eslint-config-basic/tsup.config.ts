1338c3b55d14be09272b2a9294250fa803701815
LoTwT <709937065@qq.com>
2023-08-28 17:57:29 +0800

    chore: remove verbose banner in tsup.config.ts

--- packages/eslint-config-basic/tsup.config.ts
+++ packages/eslint-config-basic/tsup.config.ts
@@ -6,10 +6,4 @@ export default defineConfig({
   clean: true,
   target: "esnext",
   dts: true,
-
-  banner: {
-    js: `const require = (await import("node:module")).createRequire(import.meta.url);
-const __filename = (await import("node:url")).fileURLToPath(import.meta.url);
-const __dirname = (await import("node:path")).dirname(__filename);`,
-  },
 })
