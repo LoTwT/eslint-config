176ebd4c5fa05bf5c12d677a207dc9fc59dcdab1
LoTwT <709937065@qq.com>
2023-09-01 09:59:08 +0800

    fix(eslint-config-markdown): match plugins key and rules namespace

--- packages/eslint-config-markdown/shims.d.ts
+++ packages/eslint-config-markdown/shims.d.ts
@@ -5,3 +5,9 @@ declare module "@sxzz/eslint-config" {
 
   export { markdown }
 }
+
+declare module "@typescript-eslint/eslint-plugin"
+declare module "eslint-plugin-import"
+declare module "eslint-plugin-unused-imports"
+declare module "eslint-plugin-antfu"
+declare module "eslint-plugin-n"
--- packages/eslint-config-markdown/src/markdown.ts
+++ packages/eslint-config-markdown/src/markdown.ts
@@ -1,10 +1,30 @@
 import { markdown as SxzzMarkdown } from "@sxzz/eslint-config"
-import { FlatESLintConfigItem } from "eslint-define-config"
+import { type FlatESLintConfigItem } from "eslint-define-config"
+import tsPlugin from "@typescript-eslint/eslint-plugin"
+import importPlugin from "eslint-plugin-import"
+import unusedImportsPlugin from "eslint-plugin-unused-imports"
+import antfuPlugin from "eslint-plugin-antfu"
+import nPlugin from "eslint-plugin-n"
 
 export const markdown: FlatESLintConfigItem[] = [
   ...SxzzMarkdown,
   {
+    files: ["**/*.md/*.*"],
+    plugins: {
+      "@typescript-eslint": tsPlugin,
+      import: importPlugin,
+      "unused-imports": unusedImportsPlugin,
+      antfu: antfuPlugin,
+      n: nPlugin,
+    },
     rules: {
+      "no-alert": "off",
+      "no-console": "off",
+      "no-restricted-imports": "off",
+      "no-undef": "off",
+      "no-unused-expressions": "off",
+      "no-unused-vars": "off",
+
       "@typescript-eslint/no-redeclare": "off",
       "@typescript-eslint/no-unused-vars": "off",
       "@typescript-eslint/no-use-before-define": "off",
@@ -13,17 +33,15 @@ export const markdown: FlatESLintConfigItem[] = [
       "@typescript-eslint/consistent-type-imports": "off",
       "@typescript-eslint/no-namespace": "off",
       "@typescript-eslint/no-require-imports": "off",
+
       "import/no-unresolved": "off",
+
       "unused-imports/no-unused-imports": "off",
       "unused-imports/no-unused-vars": "off",
-      "no-alert": "off",
-      "no-console": "off",
-      "no-restricted-imports": "off",
-      "no-undef": "off",
-      "no-unused-expressions": "off",
-      "no-unused-vars": "off",
+
       "antfu/no-cjs-exports": "off",
       "antfu/no-ts-export-equal": "off",
+
       "n/prefer-global/process": "off",
     },
   },
