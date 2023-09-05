3cd62205f97f4024bde31b9ca02948f30d7671f4
LoTwT <709937065@qq.com>
2023-09-05 10:32:02 +0800

    feat(eslint-config-basic): @typescript-eslint off some rules

--- packages/eslint-config-basic/src/ts.ts
+++ packages/eslint-config-basic/src/ts.ts
@@ -19,6 +19,21 @@ export const ts: FlatESLintConfigItem[] = [
       ...tsPlugin.configs["eslint-recommended"].overrides?.[0].rules,
       ...tsPlugin.configs.strict.rules,
       "@typescript-eslint/quotes": ["error", "double", { avoidEscape: true }],
+
+      "@typescript-eslint/consistent-indexed-object-style": "off",
+      "@typescript-eslint/naming-convention": "off",
+      "@typescript-eslint/explicit-function-return-type": "off",
+      "@typescript-eslint/explicit-member-accessibility": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/parameter-properties": "off",
+      "@typescript-eslint/no-empty-interface": "off",
+      "@typescript-eslint/ban-ts-ignore": "off",
+      "@typescript-eslint/no-empty-function": "off",
+      "@typescript-eslint/no-non-null-assertion": "off",
+      "@typescript-eslint/explicit-module-boundary-types": "off",
+      "@typescript-eslint/triple-slash-reference": "off",
+
+      // handled by unused-imports/no-unused-imports
       "@typescript-eslint/no-unused-vars": "off",
     },
   },
