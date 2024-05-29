57ede46e37180f35d04b32f4ad1dffe178c13854
Lo <l709937065@gmail.com>
2024-05-29 14:21:36 +0800

    feat: update exports

---
+++ packages/eslint-config/antfu.ts
@@ -0,0 +1,15 @@
+import { antfu } from "@antfu/eslint-config"
+import { prettier } from "@ayingott/eslint-config-prettier"
+
+export const defineConfig: typeof antfu = (args) =>
+  antfu({
+    ...args,
+    stylistic: {
+      quotes: "double",
+      ...(typeof args?.stylistic === "boolean" ? {} : args?.stylistic),
+    },
+    rules: {
+      "style/arrow-parens": "off",
+      ...args?.rules,
+    },
+  }).append(prettier)
--- packages/eslint-config/tsup.config.ts
+++ packages/eslint-config/tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "tsup"
 
 export default defineConfig({
-  entry: ["index.ts", "port.ts"],
+  entry: ["index.ts", "antfu.ts"],
   format: ["esm", "cjs"],
   target: "esnext",
   clean: true,
